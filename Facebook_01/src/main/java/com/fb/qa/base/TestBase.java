c7df825593bd336bd7ebc2831607ec47dec4ced2
91910 <91910@Dheeraj_Shet>
2023-02-07 12:25:55 +0530

    Commit-Added Code

---
+++ Facebook_01/src/main/java/com/fb/qa/base/TestBase.java
@@ -0,0 +1,19 @@
+package com.fb.qa.base;
+
+import java.util.Properties;
+
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+public class TestBase 
+{
+	WebDriver driver;
+	Logger logs;
+	Properties prop;
+	
+	public void setup()
+	{
+		
+	}
+
+}
